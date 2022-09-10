e1637b89a36123dac7cdf1f5a62be2359d868e68
malogalbo <mlogalbo@ufl.edu>
2022-09-10 17:23:50 -0400

    new model & swapped pdf for png in media
    includes model in reader\models.py & new image to test with

--- reader/admin.py
+++ reader/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import DocImage
 
 # Register your models here.
+
+class BookAdmin(admin.ModelAdmin):
+    readonly_fields = ('id', )
+
+
+admin.site.register(DocImage, BookAdmin)
--- reader/models.py
+++ reader/models.py
@@ -5,3 +5,11 @@
 #     descriptoin = models.CharField(max_length=255, blank=True)
 #     document = models.FileField(upload_to='documents/')
 #     uploaded_at = models.DateTimeField(auto_now_add=True)
+
+
+from django.db import models
+
+class DocImage(models.Model):
+    name = models.CharField(max_length=1024)
+    image = models.ImageField(upload_to='imLiterate/media')
+
