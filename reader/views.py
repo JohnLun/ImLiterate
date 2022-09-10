8b6c0b58336cbc20d987990e5375e74d3db42882
Chase Wrenn <chasewrenn@ufl.edu>
2022-09-10 10:39:32 -0400

    login/logout/register

--- reader/views.py
+++ reader/views.py
@@ -1,6 +1,31 @@
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render
+from django.shortcuts import HttpResponse
+from datetime import datetime
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic.edit import CreateView
+from django.shortcuts import redirect
 
 # Create your views here.
 class HomeView(TemplateView):
-    template_name = 'base.html'
+    template_name = 'reader/home.html'
+
+class SignUpView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'reader/register.html'
+    success_url = ''
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect('', )
+        return super().get(request, *args, **kwargs)
+
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'reader/logout.html'
+
+
+class LoginInterfaceView(LoginView):
+    template_name = 'reader/login.html'
